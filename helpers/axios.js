b195299e0b4fb55b8803ee2179bac3a43d15887e
markothedev <markothedevmail@gmail.com>
2023-09-16 20:59:46 +0100

    updated api url

--- helpers/axios.js
+++ helpers/axios.js
@@ -3,11 +3,11 @@ import * as axiosOriginal from 'axios';
 
 //this app is only for anni so just hard code the short name here
 export const tenantName = 'nimn'
-export const sitename = 'localhost:8000'
-export const baseURL= `http://localhost:8000/tenant/${tenantName}`
+// export const sitename = 'localhost:8000'
+// export const baseURL= `http://localhost:8000/tenant/${tenantName}`
 
-// export const sitename = 'rel8backend-production.up.railway.app'
-// export const baseURL= `https://rel8backend-production.up.railway.app/tenant/${tenantName}`
+export const sitename = 'rel8backend-production-adfb.up.railway.app'
+export const baseURL= `https://rel8backend-production-adfb.up.railway.app/tenant/${tenantName}`
 const axios = axiosOriginal.create({
     baseURL,
 })
